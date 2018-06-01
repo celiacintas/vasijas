6f68f34a250436d46c441111d764cba2b0aefaba
Celia Cintas <cintas.celia@gmail.com>
2018-06-01 10:13:17 -0300

    utils uploaded and classif

---
+++ utils/utils.py
@@ -0,0 +1,148 @@
+import os, gzip, torch
+import torch.nn as nn
+import numpy as np
+import scipy.misc
+import imageio
+import bisect
+import warnings
+import pandas as pd
+import shutil
+from torch._utils import _accumulate
+from torch import randperm, utils
+import matplotlib.pyplot as plt
+from torchvision import datasets, transforms
+
+
+class Subset(utils.data.Dataset):
+    """
+    Subset of a dataset at specified indices.
+
+    Arguments:
+        dataset (Dataset): The whole Dataset
+        indices (sequence): Indices in the whole set selected for subset
+    """
+    def __init__(self, dataset, indices):
+        self.dataset = dataset
+        self.indices = indices
+
+    def __getitem__(self, idx):
+        return self.dataset[self.indices[idx]]
+
+    def __len__(self):
+        return len(self.indices)
+
+
+def random_split(dataset, lengths):
+    """
+    Randomly split a dataset into non-overlapping new datasets of given lengths.
+
+    Arguments:
+        dataset (Dataset): Dataset to be split
+        lengths (sequence): lengths of splits to be produced
+    """
+    if sum(lengths) != len(dataset):
+        raise ValueError("Sum of input lengths does not equal the length of the input dataset!")
+
+    indices = randperm(sum(lengths))
+    return [Subset(dataset, indices[offset - length:offset]) for offset, length in zip(_accumulate(lengths), lengths)]
+
+
+def print_network(net):
+    num_params = 0
+    for param in net.parameters():
+        num_params += param.numel()
+    print(net)
+    print('Total number of parameters: %d' % num_params)
+
+def save_images(images, size, image_path):
+    return imsave(images, size, image_path)
+
+def imsave(images, size, path):
+    image = np.squeeze(merge(images, size))
+    return scipy.misc.imsave(path, image)
+
+def merge(images, size):
+    h, w = images.shape[1], images.shape[2]
+    if (images.shape[3] in (3,4)):
+        c = images.shape[3]
+        img = np.zeros((h * size[0], w * size[1], c))
+        for idx, image in enumerate(images):
+            i = idx % size[1]
+            j = idx // size[1]
+            img[j * h:j * h + h, i * w:i * w + w, :] = image
+        return img
+    elif images.shape[3]==1:
+        img = np.zeros((h * size[0], w * size[1]))
+        for idx, image in enumerate(images):
+            i = idx % size[1]
+            j = idx // size[1]
+            img[j * h:j * h + h, i * w:i * w + w] = image[:,:,0]
+        return img
+    else:
+        raise ValueError('in merge(images,size) images parameter ''must have dimensions: HxW or HxWx3 or HxWx4')
+
+def generate_animation(path, num):
+    images = []
+    for e in range(num):
+        img_name = path + '_epoch%03d' % (e+1) + '.png'
+        images.append(imageio.imread(img_name))
+    imageio.mimsave(path + '_generate_animation.gif', images, fps=5)
+
+def loss_plot(hist, path = 'Train_hist.png', model_name = ''):
+    x = range(len(hist['D_loss']))
+
+    y1 = hist['D_loss']
+    y2 = hist['G_loss']
+
+    plt.plot(x, y1, label='D_loss')
+    plt.plot(x, y2, label='G_loss')
+
+    plt.xlabel('Iter')
+    plt.ylabel('Loss')
+
+    plt.legend(loc=4)
+    plt.grid(True)
+    plt.tight_layout()
+
+    path = os.path.join(path, model_name + '_loss.png')
+
+    plt.savefig(path)
+
+    plt.close()
+
+def initialize_weights(net):
+    for m in net.modules():
+        if isinstance(m, nn.Conv2d):
+            m.weight.data.normal_(0, 0.02)
+            m.bias.data.zero_()
+        elif isinstance(m, nn.ConvTranspose2d):
+            m.weight.data.normal_(0, 0.02)
+            m.bias.data.zero_()
+        elif isinstance(m, nn.Linear):
+            m.weight.data.normal_(0, 0.02)
+            m.bias.data.zero_()
+
+def create_df_from_files(path='data/perfiles_CATA/clases/'):
+    l = list()  
+    for class_, filename in enumerate(os.listdir(path), 1):
+        with open(os.path.join(path, filename)) as f:
+            lines = f.readlines()
+            for id_ in lines:
+                l.append((id_.rstrip(), class_))
+    df_classes = pd.DataFrame(l, columns=['id', 'class'])
+
+    return df_classes
+
+def create_folder_pytorch_format(df, destination, path):
+    for row in df.iterrows():
+        directory = os.path.join(destination, str(row[1][1]))
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        name = row[1][0] + '.png'
+        for root, dirs, files in os.walk(path):
+            if name in files:
+                print(os.path.join(root, name))
+                shutil.copy(os.path.join(root, name), destination + str(row[1][1]))
+
+if __name__ == "__main__":
+    pass
