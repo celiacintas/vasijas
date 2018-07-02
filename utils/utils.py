27607b06f73bbf0b833ecd4c2ab0cd9d8f9258d2
Celia Cintas <cintas.celia@gmail.com>
2018-07-02 09:01:43 -0300

    tsne cleanup

--- utils/utils.py
+++ utils/utils.py
@@ -9,10 +9,13 @@ import pandas as pd
 import shutil
 from torch._utils import _accumulate
 from torch import randperm, utils
-import matplotlib.pyplot as plt
 from torchvision import datasets, transforms
 from PIL import Image
+import PIL.ImageOps
+import matplotlib.pyplot as plt
 from matplotlib import offsetbox
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import proj3d
 from sklearn import manifold
 
 
@@ -153,6 +156,48 @@ def create_folder_pytorch_format(df, destination, path):
 def create_plot_window(vis, xlabel, ylabel, title):
     return vis.line(X=np.array([1]), Y=np.array([np.nan]), opts=dict(xlabel=xlabel, ylabel=ylabel, title=title))
 
+def proj(X, ax, ax2d):
+        """ From a 3D point in axes ax1, 
+            calculate position in 2D in ax2 """
+        x,y,z = X
+        x2, y2, _ = proj3d.proj_transform(x,y,z, ax.get_proj())
+        tr = ax.transData.transform((x2, y2))
+        return ax2d.transData.inverted().transform(tr)
+
+
+def plot_tsne_3D(X_tsne, merged, azim=120, distance=70000):
+    fig = plt.figure(figsize=(20, 20))
+    ax = fig.add_subplot(111, projection=Axes3D.name)
+    ax2d = fig.add_subplot(111,frame_on=False) 
+    ax2d.axis("off")
+    ax.view_init(elev=30., azim=azim)
+    for i in range(X_tsne.shape[0]):
+            ax.scatter(X_tsne[i, 0], X_tsne[i, 1], X_tsne[i, 2], c=plt.cm.magma(merged.iloc[i][1] / 11.), s=100)
+    if hasattr(offsetbox, 'AnnotationBbox'):
+        shown_images = np.array([[1., 1., 1.]])
+        for i in range(merged.shape[0]):           
+            dist = np.sum((X_tsne[i] - shown_images) ** 2, 1)
+
+            if np.min(dist) < distance:
+                # don't show points that are too close
+                continue
+
+            shown_images = np.r_[shown_images, [X_tsne[i]]]
+            image =  Image.open('data/perfiles_CATA/png_full/' + merged.iloc[i][0] + '.png')
+            inverted_image = PIL.ImageOps.invert(image)
+            inverted_image.thumbnail((40, 40), Image.ANTIALIAS)
+            imagebox = offsetbox.AnnotationBbox(
+                offsetbox.OffsetImage(inverted_image),
+                proj(X_tsne[i], ax, ax2d))
+            ax2d.add_artist(imagebox)
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    plt.title('t-SNE over the 11 classes of vessels')
+    plt.savefig("/tmp/movie%d.png" % azim)
+
+# Quizas deberia eliminar 3d o limpiar
 def plot_embedding(X, merged, title=None):
     x_min, x_max = np.min(X, 0), np.max(X, 0)
     X = (X - x_min) / (x_max - x_min)
@@ -163,20 +208,21 @@ def plot_embedding(X, merged, title=None):
         plt.text(X[i, 0], X[i, 1], str(merged.iloc[i][1]),
                  color=plt.cm.Set1(merged.iloc[i][1] / 10.),
                  fontdict={'weight': 'bold', 'size': 9})
-
+            
     if hasattr(offsetbox, 'AnnotationBbox'):
-        # only print thumbnails with matplotlib > 1.0
-        shown_images = np.array([[1., 1.]])  # just something big
-        for i in range(merged.shape[0]):
+        shown_images = np.array([[1., 1.]])
+        for i in range(merged.shape[0]):           
             dist = np.sum((X[i] - shown_images) ** 2, 1)
-            if np.min(dist) < 4e-3:
+            if np.min(dist) < 6e-3:
                 # don't show points that are too close
                 continue
+
             shown_images = np.r_[shown_images, [X[i]]]
             image =  Image.open('data/perfiles_CATA/png_full/' + merged.iloc[i][0] + '.png')
-            image.thumbnail((28, 28), Image.ANTIALIAS)
+            inverted_image = PIL.ImageOps.invert(image)
+            inverted_image.thumbnail((28, 28), Image.ANTIALIAS)
             imagebox = offsetbox.AnnotationBbox(
-                offsetbox.OffsetImage(image, cmap=plt.cm.gray_r),
+                offsetbox.OffsetImage(inverted_image),
                 X[i])
             ax.add_artist(imagebox)
     plt.xticks([]), plt.yticks([])
