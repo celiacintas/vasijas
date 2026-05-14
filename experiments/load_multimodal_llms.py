1a15f659bec107dff570fba8edbcbb4eaf46c446
Celia Cintas <cintas.celia@gmail.com>
2026-05-14 16:35:17 +0300

    images samples

--- experiments/load_multimodal_llms.py
+++ experiments/load_multimodal_llms.py
@@ -107,11 +107,14 @@ def infer_gemma3(model, processor, image, prompt, device):
             ],
         }
     ]
-    inputs = processor.apply_chat_template(
-        messages, add_generation_prompt=True, tokenize=True, return_tensors="pt"
-    ).to(device)
+    text = processor.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+    inputs = processor(text=text, images=image, return_tensors="pt").to(device)
     output = model.generate(**inputs, max_new_tokens=128)
-    return processor.decode(output[0], skip_special_tokens=True)
+    return processor.decode(
+        output[0][inputs["input_ids"].shape[1] :], skip_special_tokens=True
+    )
 
 
 # def load_moondream2(device, dtype):
