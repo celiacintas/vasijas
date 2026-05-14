aa2cba2091a2e3ef4cf29105db7fb7ee68f6a9bd
Celia Cintas <cintas.celia@gmail.com>
2026-05-14 16:37:54 +0300

    images samples

--- experiments/load_multimodal_llms.py
+++ experiments/load_multimodal_llms.py
@@ -111,6 +111,8 @@ def infer_gemma3(model, processor, image, prompt, device):
         messages, tokenize=False, add_generation_prompt=True
     )
     inputs = processor(text=text, images=image, return_tensors="pt").to(device)
+    if "pixel_values" in inputs:
+        inputs["pixel_values"] = inputs["pixel_values"].to(model.dtype)
     output = model.generate(**inputs, max_new_tokens=128)
     return processor.decode(
         output[0][inputs["input_ids"].shape[1] :], skip_special_tokens=True
