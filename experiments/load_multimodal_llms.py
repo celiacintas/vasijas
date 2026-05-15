476a5018e96c7f15083b6ac54c3d52be7831661c
Celia Cintas <cintas.celia@gmail.com>
2026-05-15 16:49:06 +0300

    dataset refs

--- experiments/load_multimodal_llms.py
+++ experiments/load_multimodal_llms.py
@@ -34,7 +34,7 @@ def infer_llava(model, processor, image, prompt, device):
         messages, tokenize=False, add_generation_prompt=True
     )
     inputs = processor(images=image, text=text, return_tensors="pt").to(device)
-    output = model.generate(**inputs, max_new_tokens=128)
+    output = model.generate(**inputs, max_new_tokens=10)
     return processor.decode(
         output[0][inputs["input_ids"].shape[1] :], skip_special_tokens=True
     )
